384266911ba9e9fe0806defd80e3f9e6b5332686
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-17 18:31:17 -0600

    Add the functions in the html

--- figuras.js
+++ figuras.js
@@ -1,3 +1,4 @@
+//Funciones----------------------------------------------------------------
 //Código cuadrado
 function perimetroCuadrado(lado) {
   return lado * 4;
@@ -29,4 +30,47 @@ function perimetroCirculo(radio) {
 
 function areaCirculo(radio) {
   return (radio * radio) * pi;
+}
+
+//Código HTML--------------------------------------------------------------
+//Cuadrado
+function calculaPerimetroCuadrado() {
+  const input = parseInt(document.getElementById("inputCuadrado").value);
+  const perimetro = perimetroCuadrado(input);
+  alert(perimetro);
+}
+
+function calculaAreaCuadrado() {
+  const input = parseInt(document.getElementById("inputCuadrado").value);
+  const area = areaCuadrado(input);
+  alert(area);
+}
+
+//Triángulo
+function calculaPerimetroTriangulo() {
+  const input1 = parseInt(document.getElementById("primerLado").value);
+  const input2 = parseInt(document.getElementById("segundoLado").value);
+  const input3 = parseInt(document.getElementById("base").value);
+  const perimetro = perimetroTriangulo(input1, input2, input3);
+  alert(perimetro);
+}
+
+function calculaAreaTriangulo() {
+  const input1 = parseInt(document.getElementById("base").value);
+  const input2 = parseInt(document.getElementById("altura").value);
+  const area = areaTriangulo(input1, input2);
+  alert(area);
+}
+
+//Círculo
+function calculaPerimetroCirculo() {
+  const input1 = parseInt(document.getElementById("radio").value);
+  const perimetro = perimetroCirculo(input1);
+  alert(perimetro);
+}
+
+function calculaAreaCirculo() {
+  const input1 = parseInt(document.getElementById("radio").value);
+  const area = areaCirculo(input1);
+  alert(area);
 }
