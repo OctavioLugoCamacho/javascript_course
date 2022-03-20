47d6cbeca25a43b51f9bc19fb524bf4ebd2f4dd3
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-20 13:39:56 -0600

    Add dynamic functions and sections

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,11 @@ function calcularPrecioConDescuento(precio, descuento) {
   const precioDescuento = (precio * porcentajeDescuento) / 100;
   return precioDescuento;
 }
+
+function calcularPrecio() {
+  const inputPrice = parseInt(document.getElementById("price").value);
+  const inputDiscount = parseInt(document.getElementById("discount").value);
+  const price = calcularPrecioConDescuento(inputPrice, inputDiscount);
+  const result = document.getElementById("result");
+  result.innerText = "El precio con descuento es: $" + price;
+}
