4c67c23d0ce9cb82a76ed5889820cf9f07a70ed3
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-20 19:52:08 -0600

    Add switch for coupons

--- descuentos.js
+++ descuentos.js
@@ -11,3 +11,31 @@ function calcularPrecio() {
   const result = document.getElementById("result");
   result.innerText = "El precio con descuento es: $" + price;
 }
+
+const coupons = [
+  "Descuento 1",
+  "Descuento 2",
+  "Descuento 3",
+];
+
+const coupon = document.getElementById("coupon").value;
+
+function calcularDescuento() {
+  const inputPrice = parseInt(document.getElementById("price").value);
+  const coupon = document.getElementById("coupon").value;
+  let descuento;
+  switch(coupon) {
+    case coupons[0]: //Descuento 1
+      descuento = 15;
+    break;
+    case coupons[1]: //Descuento 2
+      descuento = 30;
+    break;
+    case coupons[2]: //Descuento 3
+      descuento = 25;
+    break;
+  }
+  const precioConDescuento = calcularPrecioConDescuento(inputPrice, descuento);
+  const result = document.getElementById("result");
+  result.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
