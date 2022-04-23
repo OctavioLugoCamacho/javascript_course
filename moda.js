9e04ca6e49eb7ff4e52b3264f03843a65428e4d4
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-22 20:21:04 -0500

    Add the function to calculate moda

---
+++ moda.js
@@ -0,0 +1,20 @@
+const list = [ 1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1 ];
+const listCount = {};
+
+list.map(
+  function(element) {
+    if (listCount[element]) {
+      listCount[element] += 1;
+    } else {
+      listCount[element] = 1;
+    }
+  }
+);
+
+const array = Object.entries(listCount).sort(
+  function(value1, value2) {
+    return value1[1] - value2[1];
+  }
+);
+
+const moda = array[array.length - 1];
